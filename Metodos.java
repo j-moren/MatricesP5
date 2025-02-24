e764e1e1f5440a776a70deefe6f278ebfa4989d4
J-Moren <kalev.1503@gmail.com>
2025-02-23 19:57:00 -0500

    Se crea Método Solicitar dimensión

--- Metodos.java
+++ Metodos.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Metodos {
     
+    public int[][] solicitarDimensión() {
+        Scanner sc = new Scanner(System.in);// Crear Objeto para Leer Entrada de datos
+
+        System.out.println("Ingrese el Número de Filas de la Matriz: ");// Solicitar el Número de Filas
+        int i = sc.nextInt();// Lee el Número de Filas
+
+        System.out.println("Ingrese el Número de Columnas: ");// Solicitar el Número de Columnas
+        int j = sc.nextInt();// Lee el Número de Columnas
+
+        int [][] matriz = new int[i][j];// Crea Matriz con las Dimensiones Ingresadas
+
+        sc.close();
+
+    }
 }
